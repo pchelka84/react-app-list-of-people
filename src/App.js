2633628d2c84da13d1d13e6a8a5d54688dc6bb41
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-12 15:55:26 -0500

    Creating switchNameHandler method to handle onClick event

--- src/App.js
+++ src/App.js
@@ -11,13 +11,24 @@ class App extends Component {
     ]
   };
 
+  switchNameHandler = () => {
+    // console.log("Was clicked");
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Jane", age: 34 },
+        { name: "Jake", age: 27 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <p>This is really working!</p>
 
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
 
         <Person
           name={this.state.persons[0].name}
@@ -31,7 +42,7 @@ class App extends Component {
         </Person>
         <Person
           name={this.state.persons[2].name}
-          age={this.state.persons[0].age}
+          age={this.state.persons[2].age}
         />
       </div>
     );
