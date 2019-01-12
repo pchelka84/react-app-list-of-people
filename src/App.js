9b92ba2b311eddbaf605515d070c9dff821a091f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-12 15:41:27 -0500

    Adding status & passing it to the component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Jane", age: 34 },
+      { name: "Jake", age: 32 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Jane" age="24">
+
+        <button>Switch name</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My hobbies: reading
         </Person>
-        <Person name="Jake" age="32" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[0].age}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,6 +6,7 @@ const person = props => {
       <p>
         I'm a {props.name} and I'm {props.age} years old!
       </p>
+      <p>{props.children}</p>
     </div>
   );
 };
