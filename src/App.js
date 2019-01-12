b262b3e4d941ed7d83939ef91eea4fb5d52a5d0a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-12 16:18:44 -0500

    Handling input field by adding two way binding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 34 }, // value a user entered
+        { name: "Jake", age: 32 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -40,6 +50,7 @@ class App extends Component {
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, "Max!!")}
+          changed={this.nameChangedHandler}
         >
           My hobbies: reading
         </Person>
