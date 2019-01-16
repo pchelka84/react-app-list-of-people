e423692204648667320accacf1ba27c836a14978
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-16 15:33:53 -0500

    Utilizing PureComponent

--- src/components/Persons/Persosns.js
+++ src/components/Persons/Persosns.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log("[Persons.js] Inside Constructor");
@@ -22,19 +22,19 @@ class Persons extends Component {
     );
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(
-      "[UPDATE Persons.js Inside shouldComponentUpdate",
-      nextProps,
-      nextState
-    );
-    return (
-      nextProps.persons !== this.props.persons ||
-      nextProps.changed !== this.props.changed ||
-      nextProps.clicked !== this.props.clicked
-    );
-    // return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(
+  //     "[UPDATE Persons.js Inside shouldComponentUpdate",
+  //     nextProps,
+  //     nextState
+  //   );
+  //   return (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   );
+  //   // return true;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log(
