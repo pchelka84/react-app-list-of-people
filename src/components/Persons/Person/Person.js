6aabe2e7a5b184d8522769c0af283e8bdec8377a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-16 11:37:39 -0500

    Converting Person & Persons into stateful functional componenets

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Person.css";
 import classes from "./Person.css";
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm a {props.name} and I'm {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  state = {};
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name} and I'm {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
