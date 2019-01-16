580a930beac4bb90c3598a807912491e55382b41
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-16 12:09:36 -0500

    Adding Component creation  lifecycles to Persons & Person

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,22 @@ import "./Person.css";
 import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[Person.js] Inside Constructor");
+  }
+
+  componentWillMount() {
+    console.log("[Person.js] Inside componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("[Person.js] Inside componentDidMount");
+  }
+
   render() {
+    console.log("[Person.js] Inside render()");
+
     return (
       <div className={classes.Person}>
         <p onClick={this.props.click}>
