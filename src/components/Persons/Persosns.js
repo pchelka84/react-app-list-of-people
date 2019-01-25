9f0146d9ea511b899f081336f1bfba4fd06e2b04
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-25 10:52:28 -0500

    Using forwardRef

--- src/components/Persons/Persosns.js
+++ src/components/Persons/Persosns.js
@@ -5,6 +5,7 @@ class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log("[Persons.js] Inside Constructor");
+    this.lastPersonRef = React.createRef();
   }
 
   componentWillMount() {
@@ -13,6 +14,7 @@ class Persons extends PureComponent {
 
   componentDidMount() {
     console.log("[Persons.js] Inside componentDidMount");
+    this.lastPersonRef.current.focus();
   }
 
   componentWillReceiveProps(nextProps) {
@@ -61,6 +63,7 @@ class Persons extends PureComponent {
           click={() => this.props.clicked(index)}
           key={person.id}
           name={person.name}
+          ref={this.lastPersonRef}
           position={index}
           age={person.age}
           changed={event => this.props.changed(event, person.id)}
